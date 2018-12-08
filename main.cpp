f8a5a86746e86b733c7b12356aa02d7344b5227f
Ilia Komarov <nexen505@yandex.ru>
2018-12-08 23:06:43 +0400

    Added check of search results identity.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <cassert>
 #include "store/vector/VectorOfVectors.h"
 #include "vectorGenerators/CommandLineGenerator.h"
 #include "vectorGenerators/RandomVectorGenerator.h"
@@ -10,6 +11,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+//    int count = 1000000, dimension = 3;
     int count = 0, dimension = 0;
     printf("Input vectors count: ");
     scanf("%d", &count);
@@ -20,6 +22,7 @@ int main(int argc, char *argv[]) {
     double vvDuration = 0.0, kdDuration = 0.0;
     RandomVectorGenerator vectorGenerator;
     VectorStore *store;
+    vector<double> *searchResult, vvSearchResult, kdTreeSearchResult;
 
     for (int i = 0; i < testsCount; i++) {
         // Generating list and vector to test search
@@ -31,7 +34,10 @@ int main(int argc, char *argv[]) {
         store = new VectorOfVectors(generatedList, count, dimension);
         printf("VectorOfVectors: count = %d, dimension = %d\n", count, dimension);
         clock_t start = clock();
-        store->search(match);
+        searchResult = store->search(match);
+        if (searchResult) {
+            vvSearchResult = vector<double>(*searchResult);
+        }
         double duration = (clock() - start) / (double) CLOCKS_PER_SEC * 1e3;
         printf("VectorOfVectors: search time: %f ms\n", duration);
         vvDuration += duration;
@@ -41,7 +47,11 @@ int main(int argc, char *argv[]) {
         store = new KdTree(generatedList, count, dimension);
         printf("KdTree: count = %d, dimension = %d\n", count, dimension);
         start = clock();
-        store->search(match);
+        searchResult = store->search(match);
+        if (searchResult) {
+            kdTreeSearchResult = vector<double>(*searchResult);
+        }
+        assert(vvSearchResult == kdTreeSearchResult);
         duration = (clock() - start) / (double) CLOCKS_PER_SEC * 1e3;
         printf("KdTree: search time: %f ms\n", duration);
         kdDuration += duration;
