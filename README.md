fb1dfd25d1842d977a006e8c3c72af46b61924cb
Ilia Komarov <nexen505@yandex.ru>
2018-12-19 22:54:44 +0400

    Added some comments.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Useful links:
 1. https://rosettacode.org/wiki/K-d_tree#C
 2. https://rosettacode.org/wiki/Talk:K-d_tree
 3. https://ru.wikipedia.org/wiki/K-%D0%BC%D0%B5%D1%80%D0%BD%D0%BE%D0%B5_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BE
+4. https://habr.com/post/312882/
+5. https://neerc.ifmo.ru/wiki/index.php?title=%D0%9A-d_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D1%8C%D1%8F_%D0%B8_%D0%BF%D0%B5%D1%80%D0%B5%D1%87%D0%B8%D1%81%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D1%82%D0%BE%D1%87%D0%B5%D0%BA_%D0%B2_%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%BB%D1%8C%D0%BD%D0%BE%D0%BC_%D0%BF%D1%80%D1%8F%D0%BC%D0%BE%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA%D0%B5_(%D1%81%D1%82%D0%B0%D1%82%D0%B8%D0%BA%D0%B0)
--- vectorGenerators/FileGenerator.cpp
+++ vectorGenerators/FileGenerator.cpp
@@ -3,23 +3,35 @@
 #include "FileGenerator.h"
 
 FileGenerator::FileGenerator(string fileName) {
+    // open file
     ifstream in(fileName);
     if (!in) {
         throw invalid_argument("Cannot open the file : " + fileName);
     }
+
+    // first line with vector to search must be
     string str;
     if (!getline(in, str)) {
         throw invalid_argument("Cannot parse vector to search : " + fileName);
     }
+
+    // read line by stream and write into vector
     double temp;
     stringstream vectorToSearchStream(str);
     while (vectorToSearchStream >> temp) this->vectorToSearch.push_back(temp);
+
+    // reading all other lines of file as vectors
     while (getline(in, str)) {
+        // read line by stream and write into vector
         vector<double> array;
         stringstream ss(str);
         while (ss >> temp) array.push_back(temp);
+
+        // push read vector into vectors list
         this->storeContent.push_back(array);
     }
+
+    // close opened file
     in.close();
 }
 
